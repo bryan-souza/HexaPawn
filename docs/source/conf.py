a8a1b2f6158064a11d0e39696a54fc1cc29578c8
bryan-souza <bryan-souza@github.com>
2021-09-21 20:48:44 -0300

    Update v2.06 Added brazillian portuguese translation for the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,9 @@ author = 'Hellstrike12'
 
 # -- General configuration ---------------------------------------------------
 
+gettext_compact = False
+gettext_uuid = True
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
